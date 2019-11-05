6b678532d260bb0db17dbabd65d902bcf0608518
zhouyizhao <zhouyizhao8888@qq.com>
2019-11-05 19:19:16 +0800

    work

---
+++ public/precache-manifest.f7b101ecd36c3b48a461e81523600bd2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24c54435ebd30c1599cd88d4840666f4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "817d3150eae66353426e",
+    "url": "/static/css/2.802837ff.chunk.css"
+  },
+  {
+    "revision": "e05e31d9bec6a680a268",
+    "url": "/static/css/main.10e083aa.chunk.css"
+  },
+  {
+    "revision": "817d3150eae66353426e",
+    "url": "/static/js/2.92271b95.chunk.js"
+  },
+  {
+    "revision": "e05e31d9bec6a680a268",
+    "url": "/static/js/main.84ced5e5.chunk.js"
+  },
+  {
+    "revision": "628ea25a63cbc5b89a27",
+    "url": "/static/js/runtime-main.9f147cbc.js"
+  }
+]);
